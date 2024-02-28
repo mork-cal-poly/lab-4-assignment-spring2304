4e9189061fac37cb75e6f15dd8e478081062b0f1
aviv kesar <avivkesar@gmail.com>
2024-02-28 09:56:12 -0800

    added second wave of bubbles

--- src/sketch.js
+++ src/sketch.js
@@ -9,6 +9,14 @@ let x2Bubbles = 280;
 let y2Bubbles = 500;
 let x3Bubbles = 150;
 let y3Bubbles = 600;
+let x1Wave = 0;
+let y1Wave = 200;
+let x2Wave = 10;
+let y2Wave = 300;
+let x3Wave = -15;
+let y3Wave = 400;
+let x4Wave = 20;
+let y4Wave = 500;
 
 function preload() {
   img = loadImage('../assets/Paul.png');
@@ -65,6 +73,20 @@ function draw() {
   y2Bubbles = y2Bubbles -2;
   y3Bubbles = y3Bubbles -2;
  
+  if(y1Bubbles <= -10) {
+    y1Wave = y1Wave - 2;
+    x1Wave = x1Wave - 1;
+    y2Wave = y2Wave - 2;
+    x2Wave = x2Wave + 1;
+    y3Wave = y3Wave - 2;
+    x3Wave = x3Wave - 0.5;
+    y4Wave = y4Wave - 2;
+    x4Wave = x4Wave + 0.5;
+    drawWave(x1Wave,y1Wave, 1);
+    drawWave(x2Wave, y2Wave, 0.75);
+    drawWave(x3Wave, y3Wave, 1);
+    drawWave(x4Wave, y4Wave, 0.75)
+  }
   
 //Bubbles
   drawBubbles(x1Bubbles, y1Bubbles, 1);
@@ -109,6 +131,64 @@ function drawPaul(x, y) {
   push();
     translate(x, y);
     image(img, 0, 0);
+  pop();
+}
+
+function drawWave(x, y, s) {
+  push();
+  translate(x, y);
+  scale(s)
+  fill(240, 240, 250);
+  stroke(3, 101, 140);
+  strokeWeight(2);
+  ellipse(20, 425, 20);
+  ellipse(45, 425, 10);
+  ellipse(50, 425, 40);
+  ellipse(75, 425, 10);
+  ellipse(100, 425, 30);
+  ellipse(150, 425, 40);
+  ellipse(200, 425, 10);
+  ellipse(230, 425, 30);
+  ellipse(300, 425, 55);
+  ellipse(350, 425, 10);
+  ellipse(375, 425, 40);
+  ellipse(400, 425, 10);
+  ellipse(25, 550, 20);
+  ellipse(40, 550, 10);
+  ellipse(55, 550, 40);
+  ellipse(70, 550, 10);
+  ellipse(110, 550, 30);
+  ellipse(160, 550, 40);
+  ellipse(175, 550, 10);
+  ellipse(235, 550, 30);
+  ellipse(290, 550, 55);
+  ellipse(340, 550, 10);
+  ellipse(385, 550, 40);
+  ellipse(380, 550, 10);
+  ellipse(30, 675, 20);
+  ellipse(35, 675, 10);
+  ellipse(30, 675, 40);
+  ellipse(80, 675, 10);
+  ellipse(110, 675, 30);
+  ellipse(160, 675, 40);
+  ellipse(210, 675, 10);
+  ellipse(215, 675, 30);
+  ellipse(300, 675, 55);
+  ellipse(350, 675, 10);
+  ellipse(375, 675, 40);
+  ellipse(400, 675, 10);
+  ellipse(20, 700, 20);
+  ellipse(45, 700, 10);
+  ellipse(50, 700, 40);
+  ellipse(75, 700, 10);
+  ellipse(100, 700, 30);
+  ellipse(150, 700, 40);
+  ellipse(200, 700, 10);
+  ellipse(230, 700, 30);
+  ellipse(300, 700, 55);
+  ellipse(360, 700, 10);
+  ellipse(400, 700, 40);
+  ellipse(505, 700, 10);
   pop();
 }
 
